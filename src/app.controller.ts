4deaeb51efa8faee359770edc5c0c9f0d1e4c145
Marius Bongarts <mb709027@fh-muenster.de>
2019-09-13 23:34:15 +0200

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { LoginUserDto } from './users/dto/login-user.dto';
+import { UsersService } from './users/users.service';
+import { Controller, Get, Body, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { CreateUserDto } from './users/dto/create-user.dto';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+    ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/register')
+  async register(@Body() userDto: CreateUserDto) {
+    const createdUser = await this.usersService.create(userDto);
+    return createdUser;
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
